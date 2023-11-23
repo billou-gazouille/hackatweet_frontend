f25c9e773beff541874e555e24d57a7ffd58375b
William Joannes <william.joannes@outlook.com>
2023-11-23 11:31:44 +0100

    noice

---
+++ components/Login.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Login.module.css';
+
+function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
+
+export default Login;
---
+++ components/Signup.js
@@ -0,0 +1,65 @@
+import styles from '../styles/Signup.module.css';
+import Image from 'next/image';
+import logo from '../hackertweetLogo.png'
+import { useState } from 'react';
+
+function Signup() {
+
+    const [firstName, setFirstName] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');   
+
+    const handleSignupClick = async () => {
+        //console.log('handleSignupClick');
+        const resp = await fetch('http://localhost:3000/users/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({firstName, username, password}),
+        });
+        const data = await resp.json();
+        console.log(data);
+    };
+
+    const handleExitClick = () => {
+        console.log('handleExitClick');
+        
+    };
+
+    return (
+        <div className={styles.panel}>
+            <div 
+                className={styles.cross}
+                onClick={() => handleExitClick()}
+            >X
+            </div>
+            <Image src={logo} width={40} height={40}/>
+            <h2>Create your hackertweet account</h2>
+            <input 
+                className={styles.inputField} 
+                placeholder='first name' 
+                onChange={e => setFirstName(e.target.value)} 
+                value={firstName} 
+            />
+            <input 
+                className={styles.inputField} 
+                placeholder='username' 
+                onChange={e => setUsername(e.target.value)} 
+                value={username} 
+            />  
+            <input 
+                className={styles.inputField}  
+                placeholder='password' 
+                type='password' 
+                onChange={e => setPassword(e.target.value)} 
+                value={password} 
+            />
+            <button 
+                className={styles.signupButton}
+                onClick={() => handleSignupClick()}
+            >Sign up
+            </button>
+        </div>
+    );
+}
+
+export default Signup;
