d1c3d845f753d4a0a2ac4c95644e943594522076
Louis <sanson.louis@gmail.com>
2023-11-24 09:18:18 +0100

    qsd

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,15 +1,25 @@
 import styles from "../styles/Tweet.module.css";
+
 function Tweet({ user, tweet, hashtags, date }) {
-  const tweet = props.tweetData;
-  console.log(props, tweet);
+  const tweetData = tweet;
+  console.log(user, tweet, hashtags, date, tweetData);
+
   return (
     <div className={styles.tweet}>
-      tweet
+      {/* Contenu du tweet */}
       <div className={styles.userIcon}>
+        {/* Icône utilisateur */}
         <img src="chemin vers icone" alt="userIcon" />
       </div>
       <div className={styles.tweetContent}>
-        <div className={styles.tweetHeader}></div>
+        {/* Contenu du tweet (texte, média, etc.) */}
+        {tweetData}
+      </div>
+      <div className={styles.tweetHeader}>
+        {/* Header du tweet (nom d'utilisateur, hashtags, date, etc.) */}
+        <p>User: {user}</p>
+        <p>Hashtags: {hashtags}</p>
+        <p>Date: {date}</p>
       </div>
     </div>
   );
